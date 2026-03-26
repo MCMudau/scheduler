9ca9e1b31fe94af579f227e51140490f02441173
MCMudau <mudaumuthusi@gmail.com>
2026-03-26 12:25:42 +0200

    added the email sending feature to its own thread for speed when creating an account

--- src/main/java/com/mphoYanga/scheduler/services/EmailService.java
+++ src/main/java/com/mphoYanga/scheduler/services/EmailService.java
@@ -33,7 +33,10 @@ public class EmailService {
             String html = buildPinEmailHtml(adminName, pin);
             helper.setText(html, true); // true = HTML content
 
-            //mailSender.send(message);
+            new Thread(()->{
+                mailSender.send(message);
+            }).start();
+
 
         } catch (MessagingException e) {
             throw new RuntimeException("Failed to send verification email to " + toEmail, e);
